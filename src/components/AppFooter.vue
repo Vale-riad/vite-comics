090102997fae5ae8541754c2b421a7da38c953cd
Vale-riad <valentinariad91@gmail.com>
2022-11-28 11:22:02 +0100

    Aggiunge listato in AppFooter

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,12 +1,70 @@
 <script>
 export default {
   name: "AppFooter",
+  data() {
+    return {
+      links: [
+        "Characters",
+        "Comics",
+        "Movies",
+        "TV",
+        "Games",
+        "Videos",
+        "News",
+      ],
+      dcList: [
+        "Terms Of use",
+        "Privacy policy (New)",
+        "Ad Choices",
+        "Advertising",
+        "Jobs",
+        "Subscriptions",
+        "Talent Workshops",
+        "CPSC Certificates",
+        "Ratings",
+        "Shop Help",
+        "Contact Us",
+      ],
+    };
+  },
 };
 </script>
 
 <template>
   <section class="footer-top">
-    <div class="container"></div>
+    <div class="container">
+      <div class="list-left">
+        <h3>DC COMICS</h3>
+        <ul>
+          <li v-for="link in links">
+            <a href="">{{ link }}</a>
+          </li>
+        </ul>
+        <h3>SHOP</h3>
+        <ul>
+          <li><a href="#">Shop Dc</a></li>
+          <li><a href="#">Shop Dc Collectibles</a></li>
+        </ul>
+      </div>
+      <div class="list-center">
+        <h3>DC</h3>
+        <ul>
+          <li v-for="link in dcList">
+            <a href="">{{ link }}</a>
+          </li>
+        </ul>
+      </div>
+      <div class="list-right">
+        <h3>SITIES</h3>
+        <ul>
+          <li><a href="#">Dc</a></li>
+          <li><a href="#">Mad Magazine</a></li>
+          <li><a href="#">DC kids</a></li>
+          <li><a href="#">DC Universe</a></li>
+          <li><a href="#">DC Power Visa</a></li>
+        </ul>
+      </div>
+    </div>
   </section>
   <section class="footer-bottom">
     <div class="container">
@@ -35,8 +93,23 @@ export default {
     height: 100%;
     background-repeat: no-repeat;
     background-position: right;
+    display: flex;
+  }
+  h3 {
+    color: white;
+    margin-top: 50px;
+  }
+  ul {
+    list-style: none;
+    a {
+      text-decoration: none;
+      font-size: 13px;
+      padding: 15px 25px 0 0;
+      color: gray;
+    }
   }
 }
+
 .footer-bottom {
   background-color: #303030;
   padding: 20px 20px;
